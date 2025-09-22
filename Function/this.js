44a5754e47dd24232063c3e603d4a2fb5093d7f6
shaikat020 <tahsin.azad.skt@gmail.com>
2025-09-22 19:43:14 +0600

    Refractor: this

---
+++ Function/this.js
@@ -0,0 +1,8 @@
+//In JavaScript, the this keyword refers to an object.
+//The this keyword refers to different objects depending on how it is used:
+//Alone, this refers to the global object.
+//In a function, this refers to the global object.
+// In a function, in strict mode, this is undefined.
+// In an object method, this refers to the object.
+// In an event, this refers to the element that received the event.
+// Methods like call(), apply(), and bind() can refer this to any object.
