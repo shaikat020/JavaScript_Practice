5d3fc964f2198402645612e0baaead5a56e3f59d
shaikat020 <tahsin.azad.skt@gmail.com>
2025-09-26 23:19:36 +0600

    Feat: callback-2

--- promise/callback.js
+++ promise/callback.js
@@ -15,3 +15,34 @@ function mySecond() {
 
 myFirst();
 mySecond();
+
+//call a calculator function (myCalculator), and let the calculator function call the display function (myDisplayer)
+
+function myDisplayer(some) {
+  console.log(some);
+}
+
+function myCalculator1(num1, num2) {
+  let sum = num1 + num2;
+  myDisplayer(sum);
+}
+
+myCalculator1(5, 5);
+
+
+//A callback is a function passed as an argument to another function.
+//Using a callback, you could call the calculator function (myCalculator) with a callback (myCallback), and let the calculator function run the callback after the calculation is finished
+
+function myDisplayer(some) {
+  console.log(some);
+}
+
+function myCalculator(num1, num2, callback) {
+  let sum = num1 + num2;
+  if(callback) callback(sum);
+  return sum;
+}
+
+myCalculator(5, 5, myDisplayer);// When pass a function as an argument, remember not to use parenthesis.
+
+console.log(myCalculator(5,5))
