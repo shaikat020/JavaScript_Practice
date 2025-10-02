3b36365f8e51e807585cf8d9f77868ba661ac43a
shaikat020 <tahsin.azad.skt@gmail.com>
2025-10-02 22:21:29 +0600

    Feat: DOM Intro

---
+++ DOM/intro.js
@@ -0,0 +1,19 @@
+//With the HTML DOM, JavaScript can access and change all the elements of an HTML document.
+//When a web page is loaded, the browser creates a Document Object Model of the page.
+
+//With the object model, JavaScript gets all the power it needs to create dynamic HTML:
+// JavaScript can change all the HTML elements in the page
+// JavaScript can change all the HTML attributes in the page
+// JavaScript can change all the CSS styles in the page
+// JavaScript can remove existing HTML elements and attributes
+// JavaScript can add new HTML elements and attributes
+// JavaScript can react to all existing HTML events in the page
+// JavaScript can create new HTML events in the page
+
+// The HTML DOM is a standard object model and programming interface for HTML. It defines:
+
+// The HTML elements as objects
+// The properties of all HTML elements
+// The methods to access all HTML elements
+// The events for all HTML elements
+//The HTML DOM is a standard for how to get, change, add, or delete HTML elements.
